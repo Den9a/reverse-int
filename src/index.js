b5a580356d3e58348fc937c35f26d7b951e57322
Den9a <lubchukden@gmail.com>
2023-08-04 17:57:24 +0300

    refactor: update file index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function reverse (n) {
-  
+
+    let arrNum = String(n).split('');
+    if ( arrNum[0] === '-'){
+        arrNum.splice(0, 1);
+            arrNum.reverse();
+            arrNum = arrNum.join('');
+            return arrNum;
+
+        // if (arrNum[arrNum.length - 1] === '0'){
+        //     arrNum.splice(-1, 1);
+        //     arrNum.reverse();
+        //     arrNum = arrNum.join('');
+        //     return +arrNum;
+        //}
+    } else if (arrNum[arrNum.length - 1] === '0'){
+        arrNum.splice(-1, 1);
+        arrNum.reverse();
+        arrNum = arrNum.join('');
+        return arrNum;
+    } else {
+        arrNum.reverse();
+        arrNum = arrNum.join('');
+        return arrNum;
+    }
+    
 }
